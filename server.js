13a0570b3a74c5349daa3d4c689c2f0b5e60b548
LittleMagician <bthaoxaochay@gmail.com>
2021-02-26 12:53:59 -0600

    setup server and routes

---
+++ server.js
@@ -0,0 +1,32 @@
+// Modules to give our server useful functionality
+var express = require("express");
+
+// Express Server
+var app = express();
+
+// Express PORT
+var PORT = process.env.PORT || 8080;
+
+// Express handling data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//setup public folder for images, js, css
+app.use(express.static('public'));
+
+
+//ROUTES
+
+//API 
+require("./routes/apiRoutes")(app);
+
+// HTML 
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER - turns on server
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
+
+
