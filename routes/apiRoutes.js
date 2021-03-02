5357b150a9016e96a0deb19ca3a3f2680de6f9c5
LittleMagician <bthaoxaochay@gmail.com>
2021-03-01 20:23:02 -0600

    wrote code for each note to have an id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,43 +4,27 @@ const fs = require('fs');
 
 module.exports = function (app) {
 
+
+
     // Read JSON file and returns all saved notes to JSON
     app.get("/api/notes", function (req, res) {
-        res.json(noteData);
+        // Adding id to each object
+        let noteDataID = noteData.forEach((item, i) => {item.id = i + 1;});
+        res.json(noteDataID);
     });
 
     // New note return to client
     app.post("/api/notes", function (req, res) {
         
-        // All notes saved to request body (Needs to be only the new one)
-        noteData.push(req.body);
-
-        // New note added to db.json
-        fs.readFile('../Develop/db/db.json', 'utf8', function (error, data){
-            if (error){
-                console.error(error);
-            };
-        
-            let newNote = data;
-        
-            fs.writeFile('../Develop/db/db.json', newNote, 'utf8', (error) => error ? 'error' : 'Note added');
-        } )
-
-        res.json(true);
-    });
-
-    app.delete("/api/notes/:id", function (req, res) {
-        
-        let noteBody = req.body;
+        let newNoteBody = req.body;
 
         for (let i = 0; i < noteData.length; i++) {
-            if (noteData[i].id == noteBody.id) {
 
-                // Getting index of note
-                let index = noteData.indexOf(noteData[i]);
+            // Looping through to find the new note
+            if (noteData[i].id != newNoteBody.id) {
 
-                // Deleting that note
-                let newNoteData = noteData.splice(index, 1);
+                // Pushing new note to noteData JSON
+                let newNote = noteData.push(newNoteBody[i]);
 
                 // Rewriting notes to db.json
                 fs.readFile('../Develop/db/db.json', 'utf8', function (error, data){
@@ -48,7 +32,41 @@ module.exports = function (app) {
                         console.error(error);
                     };
                                 
-                    fs.writeFile('../Develop/db/db.json', newNoteData, 'utf8', (error) => error ? 'error' : 'Note deleted');
+                    fs.writeFile('../Develop/db/db.json', newNote, 'utf8', (error) => error ? 'error' : 'Note deleted');
+                }) // Read File
+                
+            } else {
+
+                console.log("This note already exists.")
+
+            } // If Statement
+        } // For Loop
+
+        res.json(true);
+    });
+
+
+
+    app.delete("/api/notes/:id", function (req, res) {
+        
+        let oldNoteBody = req.body;
+
+        for (let i = 0; i < noteData.length; i++) {
+            if (noteData[i].id == oldNoteBody.id) {
+
+                // Getting index of note
+                let index = noteData.indexOf(noteData[i]);
+
+                // Deleting that note
+                let deleteNote = noteData.splice(index, 1);
+
+                // Rewriting notes to db.json
+                fs.readFile('../Develop/db/db.json', 'utf8', function (error, data){
+                    if (error){
+                        console.error(error);
+                    };
+                                
+                    fs.writeFile('../Develop/db/db.json', deleteNote, 'utf8', (error) => error ? 'error' : 'Note deleted');
                 }) // Read File
             } // If Statement
         } // For Loop
