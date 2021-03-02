0d095a28d760530715fca6d1be85bc598b474807
LittleMagician <bthaoxaochay@gmail.com>
2021-03-02 16:26:38 -0600

    New note pushed to db.json and given back to client

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 module.exports = function (app) {
     
     // Adding id to each object
-    noteData.forEach((item) => {item.id = Math.floor(Math.random()*1000) + 1;});
+    noteData.forEach((item, i) => {item.id = i + 1;});
 
     // Read JSON file and returns all saved notes to JSON
     app.get("/api/notes", function (req, res) {
@@ -16,38 +16,42 @@ module.exports = function (app) {
     app.post("/api/notes", function (req, res) {
         
         let newNoteBody = req.body;
+        noteData.push(newNoteBody);
+        res.json(newNoteBody);
 
-        // Does the body automatically save into a JSON?
-        // If it does, how can I separate the new note from the old notes in the body?
+        // if (newNoteBody !== "") {
 
-        newNoteBody.forEach((item) => {item.id = Math.floor(Math.random()*1000) + 1;});
+        //     newNoteBody.forEach((item, i) => {item.id = i + 1;});
 
-        for (let i = 0; i < noteData.length; i++) {
+        //     for (let i = 0; i < noteData.length; i++) {
 
-            // Looping through to find the new note
-            if (noteData[i].id != newNoteBody.id) {
+        //         // Looping through to find the new note
+        //         if (noteData[i].id != newNoteBody[i].id) {
 
-                // Pushing new note to noteData JSON
-                noteData.push(newNoteBody);
+        //             // Pushing new note to noteData JSON
+        //             noteData.push(newNoteBody);
 
-                // Rewriting notes to db.json
-                fs.readFile('../Develop/db/db.json', 'utf8', function (error, data){
-                    if (error){
-                        console.error(error);
-                    };
-                                
-                    fs.writeFile('../Develop/db/db.json', noteData, 'utf8', (error) => error ? 'error' : 'Note deleted');
-                }) // Read File
+        //             // Rewriting notes to db.json
+        //             fs.readFile('../Develop/db/db.json', 'utf8', function (error, data){
+        //                 if (error){
+        //                     console.error(error);
+        //                 };
+                                    
+        //                 fs.writeFile('../Develop/db/db.json', noteData, 'utf8', (error) => error ? 'error' : 'Note deleted');
+        //             }) // Read File
 
-                // Returning noteData to client
-                res.json(noteData);
-                
-            } else {
+        //             // Returning noteData to client
+        //             res.json(noteData);
+                    
+        //         } else {
 
-                console.log("This note already exists.")
+        //             console.log("This note already exists.")
 
-            } // If Statement
-        } // For Loop
+        //         } // If Statement
+        //     } // For Loop
+        // } else {
+        //     console.log("There are no notes");
+        // };
 
     }); // app.post
 
@@ -55,7 +59,7 @@ module.exports = function (app) {
 
     app.delete("/api/notes/:id", function (req, res) {
         
-        let oldNoteBody = req.body;
+        let oldNoteBody = req.params.id;
 
         for (let i = 0; i < noteData.length; i++) {
             if (noteData[i].id == oldNoteBody.id) {
@@ -75,6 +79,10 @@ module.exports = function (app) {
                     // Sending updated noteData back to db.json
                     fs.writeFile('../Develop/db/db.json', noteData, 'utf8', (error) => error ? 'error' : 'Note deleted');
                 }) // Read File
+
+                // Sending updated noteData back to body
+                res.json(noteData);
+
             } // If Statement
         } // For Loop
 
